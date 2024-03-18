19ce4c5a03e98195ef2791c37c34f3b4673e91f7
ollie12343 <87453519+ollie12343@users.noreply.github.com>
2024-03-18 15:59:05 +1100

    Adding Required Files

---
+++ Task2.1WebHook/Task2.1WebHook.ino
@@ -0,0 +1,74 @@
+
+#include <WiFiNINA.h>
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+
+#include "DHT.h"
+
+#define DHTPIN 12
+
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+
+void setup() {
+ // Serial.begin(115200);      // Initialize serial 
+ Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+  dht.begin();
+}
+
+void loop() {
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+
+  // set the fields with the values
+  ThingSpeak.setField(1, t);
+  ThingSpeak.setField(2, h);
+
+  
+  // write to the ThingSpeak channel 
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+}
---
+++ Task2.1WebHook/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Oliver's iPhone"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "12345678"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2474394			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "YKOZXM2DGL1MBAGH"   // replace XYZ with your channel write API Key
